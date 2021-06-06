f4453659aa74668685cc4020f433cae18c9d5bc8
Jonathan Darwin <jonathanjojo38@yahoo.com>
2021-06-06 13:40:26 +0700

    add necessary comment

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,16 @@ class _LoginPageState extends State<LoginPage> {
     String username = UsernameTextField.controller.text;
     String password = PasswordTextField.controller.text;
 
+    /**
+     * When setState() is called, 
+     * it will render all the children widget below this state. 
+     * Means that the entire page will be re-rendered.
+     * 
+     * Best Practice : put the statefull widget as deep as possible
+     * To prevent re-render unnecessary widget, 
+     * for example FlutterIcon (FlutterIcon doesn't need to be rendered over and over again)
+     */ 
+
     setState(() {
       isButtonDisabled = username == '' || password == '';  
     });
@@ -62,6 +72,8 @@ class UsernameTextField extends StatelessWidget {
       margin: EdgeInsets.only(left: 24.0, right: 24.0, top: 32.0, bottom: 16.0),
       child: TextField(
         controller: controller,
+
+        // The purpose of using onChanged is to trigger the checkForm in LoginState()
         onChanged: (value) {
            _LoginPageState state = context.findAncestorStateOfType<_LoginPageState>();
            state.checkForm();
@@ -90,6 +102,8 @@ class PasswordTextField extends StatelessWidget {
       margin: EdgeInsets.only(left: 24.0, right: 24.0, bottom: 32.0),
       child: TextField(
         controller: controller,
+
+        // The purpose of using onChanged is to trigger the checkForm in LoginState()
         onChanged: (value) {
            _LoginPageState state = context.findAncestorStateOfType<_LoginPageState>();
            state.checkForm();
@@ -116,21 +130,27 @@ class LoginButton extends StatelessWidget {
     _LoginPageState state = context.findAncestorStateOfType<_LoginPageState>();
 
     return ElevatedButton(onPressed: state.isButtonDisabled ? null : () {
+
         String username = UsernameTextField.controller.text;
         String password = PasswordTextField.controller.text;
-        
-        
+          
         if(username == 'admin' && password == 'admin') {
+          // Navigate to Home Page with 'Replace Strategy'
+          // Means that the Login Page will be disappear and
+          // Home Page will be shown up.
+
           Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
         }
         else {
-          
           final snackbar = SnackBar(content: Text('Invalid username  / password'));
           ScaffoldMessenger.of(context).showSnackBar(snackbar);
         }
       }, 
       child: Text("Login"),
       style: ButtonStyle(
+        // Decide the button background color by looking at the isButtonDisabled value.
+        // isButtonDisabled value will be determined by the value inside username and password text field.
+
         backgroundColor: MaterialStateProperty.all(state.isButtonDisabled ? Colors.grey : Colors.blue)
       ),
     );
